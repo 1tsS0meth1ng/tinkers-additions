44336c3b1a395c3267b57b62ccb5da120c41c167
Brendan <brendanbath@gmail.com>
2014-07-05 20:23:05 +0200

    Revert "more things"
    This reverts commit c07db0c5aaa3c7008da2b7313e85c6f33ea7e413.

--- src/main/java/com/brendan/tinkeraddition/reference/Reference.java
+++ src/main/java/com/brendan/tinkeraddition/reference/Reference.java
@@ -5,7 +5,6 @@ public class Reference
 	public static final String MOD_ID = "tinkeraddition";
 	public static final String MOD_NAME = "Tinkers addition";
 	public static final String MOD_VERSION = "1.7.2-1.0";
-	public static final String CLIENT_PROXY_CLASS = "com.brendan.tinkeraddition.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "com.brendan.tinkeraddition.proxy.ServerProxy";
+	
 	
 }
