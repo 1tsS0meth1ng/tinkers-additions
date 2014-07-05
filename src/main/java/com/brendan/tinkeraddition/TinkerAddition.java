836e09699cb5d065fde399786897c06692d618b5
Brendan <brendanbath@gmail.com>
2014-07-05 18:29:59 +0200

    more stuff

--- src/main/java/com/brendan/tinkeraddition/TinkerAddition.java
+++ src/main/java/com/brendan/tinkeraddition/TinkerAddition.java
@@ -1,15 +1,22 @@
 package com.brendan.tinkeraddition;
 
+import com.brendan.tinkeraddition.proxy.IProxy;
+import com.brendan.tinkeraddition.reference.Reference;
+
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "tinkeraddition", name = "Tinkers addition", version = "1.7.2-1.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 public class TinkerAddition 
 {
-	@Mod.Instance
+	@Mod.Instance(Reference.MOD_ID)
 	public static TinkerAddition instance;
+	
+	@SidedProxy(clientSide = "com.brendan.tinkeraddition.proxy.ClientProxy", serverSide = "com.brendan.tinkeraddition.proxy.ServerProxy")
+	public static IProxy proxy;
 	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event)
--- src/main/java/com/brendan/tinkeraddition/reference/Reference.java
+++ src/main/java/com/brendan/tinkeraddition/reference/Reference.java
@@ -1,5 +1,10 @@
 package com.brendan.tinkeraddition.reference;
 
-public class Reference {
-
+public class Reference 
+{
+	public static final String MOD_ID = "tinkeraddition";
+	public static final String MOD_NAME = "Tinkers addition";
+	public static final String MOD_VERSION = "1.7.2-1.0";
+	
+	
 }
