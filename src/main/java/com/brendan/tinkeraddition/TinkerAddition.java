2c207ae41180f827dbc91f2b88b0a1c14d64e898
Brendan <brendanbath@gmail.com>
2014-07-06 00:00:23 +0200

    lots

--- src/main/java/com/brendan/tinkeraddition/TinkerAddition.java
+++ src/main/java/com/brendan/tinkeraddition/TinkerAddition.java
@@ -1,27 +1,31 @@
 package com.brendan.tinkeraddition;
 
+import com.brendan.tinkeraddition.handler.ConfigurationHandler;
 import com.brendan.tinkeraddition.proxy.IProxy;
 import com.brendan.tinkeraddition.reference.Reference;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+@Mod(modid = "larry", name = Reference.MOD_NAME, version = Reference.MOD_VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class TinkerAddition 
 {
 	@Mod.Instance(Reference.MOD_ID)
 	public static TinkerAddition instance;
 	
-	@SidedProxy(clientSide = "com.brendan.tinkeraddition.proxy.ClientProxy", serverSide = "com.brendan.tinkeraddition.proxy.ServerProxy")
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static IProxy proxy;
 	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		
+		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+
 	}
 	
 	@Mod.EventHandler
--- src/main/java/com/brendan/tinkeraddition/reference/Reference.java
+++ src/main/java/com/brendan/tinkeraddition/reference/Reference.java
@@ -2,10 +2,11 @@ package com.brendan.tinkeraddition.reference;
 
 public class Reference 
 {
-	public static final String MOD_ID = "tinkeraddition";
+	public static final String MOD_ID = "larry";
 	public static final String MOD_NAME = "Tinkers addition";
 	public static final String MOD_VERSION = "1.7.2-1.0";
 	public static final String CLIENT_PROXY_CLASS = "com.brendan.tinkeraddition.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.brendan.tinkeraddition.proxy.ServerProxy";
+	public static final String GUI_FACTORY_CLASS = "com.brendan.tinkeraddition.client.gui.GuiFactory";
 	
 }
